18486d2d1f8fca798730ec6f2dfb625adeda5c5b
Fattesingh Rane <ranefattesingh@hotmail.com>
2022-11-04 09:53:58 +0530

    feat: add search job api

---
+++ JobPortal.BAL/BusinessEntities/JobSearchQueryParamsBE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.BAL.BusinessEntities
+{
+    public class JobSearchQueryParamsBE
+    {
+        public string Q { get; set; }
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int? LocationID { get; set; }
+        public int? DepartmentID { get; set; }
+    }
+}
